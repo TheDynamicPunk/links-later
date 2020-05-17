9e4c3aaf4f9c480d1908756ff487acb27c286a42
Ashu <ashutoshgu18@gmail.com>
2020-05-18 04:12:30 +0530

    Fix sheets not working in heroku

--- handleForm.js
+++ handleForm.js
@@ -10,7 +10,7 @@ async function handleForm(formData) {
 
     await doc.useServiceAccountAuth({
         client_email: process.env.GOOGLE_SERVICE_ACCOUNT_EMAIL,
-        private_key: process.env.GOOGLE_PRIVATE_KEY,
+        private_key: process.env.GOOGLE_PRIVATE_KEY.replace(/\\n/g, '\n'),
     });
 
     await doc.loadInfo(); // loads document properties and worksheets
