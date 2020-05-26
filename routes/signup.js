f477e81eb014b8eed7d13286b01c59f7dac78c87
Ashu <ashutoshgu18@gmail.com>
2020-05-26 16:14:58 +0530

    Add signup route and DB integration

--- routes/signup.js
+++ routes/signup.js
@@ -1,36 +1,78 @@
 const router = require('express').Router();
+const User = require('../models/User');
 const { check, validationResult } = require('express-validator');
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+const mongoose = require('mongoose');
 
 router.post('/', [
+
+    // Validation for username
     check('username')
     .isLength({min: 6, max: 25})
     .withMessage('Username should be 6-25 characters long!')
-    .trim()
     .matches(/^[a-zA-Z0-9_]+$/)
-    .withMessage('Username should be alphanumeric, but underscores (_) are allowed'),
+    .withMessage('Username should be alphanumeric, but underscores (_) are allowed')
+    .trim()
+    .escape(),
 
+    // Validation for user's email address
     check('userEmail')
     .isLength({max: 50})
-    .trim()
     .isEmail()
     .withMessage('The given email id is not valid')
-    .normalizeEmail(),
+    .normalizeEmail()
+    .trim()
+    .escape(),
 
+    // Validation for user's password
     check('userPassword')
     .isLength({min: 6, max: 20})
-    .matches(/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)[a-zA-Z\d@$!%*#?&].{6,19}$/)
     .withMessage('Password should be 6-20 characters long')
+    .matches(/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)[a-zA-Z\d@$!%*#?&].{6,19}$/)
+    .withMessage('Password should contain 1 uppercase, 1 lowercase and a number!')
+    .trim()
+    .escape(),
 
-], (req,res) => {
+], async (req, res) => {
     console.log(req.body);
 
-    const errors = validationResult(req);
-    console.log(errors.array());
+    const validationErrors = validationResult(req);
+    console.log('Validation Errors: ', validationErrors.errors);
 
-    // console.log('after');
-    // console.log('username:', req.body.username);
-    console.log('email:', req.body.userEmail);
-    // console.log('password:', req.body.userPassword);
+    if(validationErrors.errors.length != 0)
+    {
+        return res.status(400).jsonp(validationErrors.errors);
+    }
+
+    //Check if username already exists
+    const usernameExists = await User.findOne({username: req.body.username});
+    
+    if(usernameExists)
+        return res.status(400).json([{usernameExists: true}]);
+
+    //Check if user email already exists in the DB
+    const emailExists = await User.findOne({email: req.body.userEmail});
+
+    if(emailExists)
+    {
+        return res.status(400).json([{emailExists: true}]);
+    }
+
+    // Hash the password for storing in DB
+    const salt_val = parseInt(process.env.SALT_VALUE);
+    const salt = await bcrypt.genSalt(salt_val);
+    const hashedPassword = await bcrypt.hash(req.body.userPassword, salt);
+
+    //create a new user and save it in the DB
+    const user = new User({
+        username: req.body.username,
+        email: req.body.userEmail,
+        password: hashedPassword
+    });
+
+    const savedUser = await user.save();
+    res.status(200).send(savedUser);
 });
 
 module.exports = router;
